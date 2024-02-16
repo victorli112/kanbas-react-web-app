1b649d4dc22c12fb63f53998df8e448ac9e455aa
victorli112 <victor.li.7802@gmail.com>
2024-02-16 11:22:41 -0500

    doing lab

---
+++ src/Labs/a3/JavaScript/functions/WorkingWithFunctions.tsx
@@ -0,0 +1,16 @@
+import ArrowFunctions from "./ArrowFunctions";
+import ES5Functions from "./ES5Functions";
+import ImpliedReturns from "./ImpliedReturn";
+import FunctionParenthesisAndParameters from "./FunctionParenthesisAndParameters";
+
+function WorkingWithFunctions() {
+  return (
+    <>
+      <ES5Functions />;
+      <ArrowFunctions />;
+      <ImpliedReturns />;
+      <FunctionParenthesisAndParameters />;
+    </>
+  );
+}
+export default WorkingWithFunctions;
