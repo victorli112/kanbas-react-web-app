b36ed5efdb30e1eae0674cad06c9f73558f7fc4f
victorli112 <victor.li.7802@gmail.com>
2024-02-16 11:26:31 -0500

    Revert "doing lab"
    This reverts commit 1b649d4dc22c12fb63f53998df8e448ac9e455aa.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Labs from "./Labs";
-import HelloWorld from "./Labs/a3/HelloWorld/helloworld";
-import Kanbas from "./Kanbas";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Labs />
-      <Kanbas />
-      <HelloWorld />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
