f32ad7b05c683813ae41772d39b807af9ef69922
victorli112 <victor.li.7802@gmail.com>
2024-02-22 10:09:59 -0500

    Revert "Merge branch 'main' into a3"
    This reverts commit 906b7a5fbae3931757ba86abcf29605d832f5ac9, reversing
    changes made to 06a128342247aa4c28d18ccff57ac0fda28706d5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
@@ -24,30 +23,6 @@ function App() {
         </div>
       </HashRouter>
     </>
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> main
   );
 }
 
---
+++ src/Labs/a3/JavaScript/arrays/WorkingWithArrays.tsx
@@ -0,0 +1,22 @@
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./AddingAndRemovingDataToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+
+function WorkingWithArrays() {
+  return (
+    <>
+      <ArrayIndexAndLength />
+      <AddingAndRemovingDataToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+    </>
+  );
+}
+export default WorkingWithArrays;
