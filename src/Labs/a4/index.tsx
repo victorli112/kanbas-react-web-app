a11d723e692f218ca641709101d5020dd3845633
victorli112 <victor.li.7802@gmail.com>
2024-03-03 17:28:53 -0500

    readd src

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,4 +1,34 @@
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./state_variables/BooleanStateVariables";
+import StringStateVariables from "./state_variables/StringStateVariables";
+import DateStateVariable from "./state_variables/DateStateVariable";
+import ObjectStateVariable from "./state_variables/ObjectStateVariable";
+import ArrayStateVariable from "./state_variables/ArrayStateVariable";
+import ParentStateComponent from "./shared_state/ParentStateComponent";
+
 function Assignment4() {
-  return <></>;
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return (
+    <>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+    </>
+  );
 }
 export default Assignment4;
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -10,6 +10,10 @@ function Nav() {
         A3
       </Link>
       <Link
+        className={`nav-link ${pathname.includes("a4") ? "active" : ""}`}
+        to="/Labs/a4"
+      ></Link>
+      <Link
         className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}
         to="/Kanbas"
       >
