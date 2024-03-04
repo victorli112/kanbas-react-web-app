1bc8f6ee543493b8fa9498b1fb53b36d287a0f5b
victorli112 <victor.li.7802@gmail.com>
2024-03-03 19:20:15 -0500

    finish walkthrough

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,45 +1,88 @@
 import React, { useState } from "react";
 import "./index.css";
-import { modules } from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
+import { KanbasState } from "../../store";
+
 function ModuleList() {
   const { courseId } = useParams();
-  const modulesList = modules.filter((module) => module.course === courseId);
+  const modulesList = useSelector(
+    (state: KanbasState) => state.modulesReducer.modules
+  );
+  const module = useSelector(
+    (state: KanbasState) => state.modulesReducer.module
+  );
+  const dispatch = useDispatch();
   const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+
   return (
     <>
-      {/* <!-- Add buttons here --> */}
-      {modulesList.map((module) => (
-        <li
-          className="list-group-item"
-          onClick={() => setSelectedModule(module)}
-        >
-          <div>
-            <FaEllipsisV className="me-2" />
-            {module.name}
-            <span className="float-end">
-              <FaCheckCircle className="text-success" />
-              <FaPlusCircle className="ms-2" />
-              <FaEllipsisV className="ms-2" />
-            </span>
-          </div>
-          {selectedModule._id === module._id && (
-            <ul className="list-group">
-              {module.lessons?.map((lesson) => (
-                <li className="list-group-item">
-                  <FaEllipsisV className="me-2" />
-                  {lesson.name}
-                  <span className="float-end">
-                    <FaCheckCircle className="text-success" />
-                    <FaEllipsisV className="ms-2" />
-                  </span>
-                </li>
-              ))}
-            </ul>
-          )}
+      <ul className="list-group wd-modules">
+        <li className="list-group-item">
+          <button
+            onClick={() => dispatch(addModule({ ...module, course: courseId }))}
+          >
+            Add
+          </button>
+          <button onClick={() => dispatch(updateModule(module))}>Update</button>
+          <input
+            value={module.name}
+            onChange={(e) =>
+              dispatch(setModule({ ...module, name: e.target.value }))
+            }
+          />
+          <textarea
+            value={module.description}
+            onChange={(e) =>
+              dispatch(setModule({ ...module, description: e.target.value }))
+            }
+          />
         </li>
-      ))}
+      </ul>
+      {modulesList
+        .filter((module) => module.course === courseId)
+        .map((module) => (
+          <li
+            className="list-group-item"
+            onClick={() => setSelectedModule(module)}
+          >
+            <button onClick={() => dispatch(setModule(module))}>Edit</button>
+
+            <button onClick={() => dispatch(deleteModule(module._id))}>
+              Delete
+            </button>
+            <div>
+              <FaEllipsisV className="me-2" />
+              {module.name}
+              <span className="float-end">
+                <FaCheckCircle className="text-success" />
+                <FaPlusCircle className="ms-2" />
+                <FaEllipsisV className="ms-2" />
+              </span>
+            </div>
+            {selectedModule._id === module._id && (
+              <ul className="list-group">
+                {module.lessons?.map((lesson: any) => (
+                  <li className="list-group-item">
+                    <FaEllipsisV className="me-2" />
+                    {lesson.name}
+                    <span className="float-end">
+                      <FaCheckCircle className="text-success" />
+                      <FaEllipsisV className="ms-2" />
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
     </>
   );
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,34 +4,85 @@ import Courses from "./Courses";
 import KanbasNavigation from "./Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
+import { useState } from "react";
+import { courses } from "./Database";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function Kanbas() {
+  const [coursesList, setCoursesList] = useState(courses);
+  const [course, setCourse] = useState({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "course.png",
+  });
+  const addNewCourse = () => {
+    setCoursesList([
+      ...coursesList,
+      { ...course, _id: new Date().getTime().toString() },
+    ]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCoursesList(coursesList.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCoursesList(
+      coursesList.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
-    <table width="100%">
-      <tbody>
-        <tr>
-          <td valign="top" width="80px">
-            <KanbasNavigation />
-          </td>
-          <td>
-            <div className="flex-fill">
-              <Routes>
-                <Route path="/" element={<Navigate to="Dashboard" />} />
-                <Route path="/Account" element={<h1>Account</h1>} />
-                <Route path="/Dashboard" element={<Dashboard />} />
-                <Route path="/Courses/:courseId/*" element={<Courses />} />
-                <Route path="/Calendar" element={<h1>Calendar</h1>} />
-                <Route path="/Inbox" element={<h1>Inbox</h1>} />
-                <Route path="/History" element={<h1>History</h1>} />
-                <Route path="/Studio" element={<h1>Studio</h1>} />
-                <Route path="/Commons" element={<h1>Commons</h1>} />
-                <Route path="/Help" element={<h1>Help</h1>} />
-              </Routes>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <Provider store={store}>
+      <table width="100%">
+        <tbody>
+          <tr>
+            <td valign="top" width="80px">
+              <KanbasNavigation />
+            </td>
+            <td>
+              <div className="flex-fill">
+                <Routes>
+                  <Route path="/" element={<Navigate to="Dashboard" />} />
+                  <Route path="/Account" element={<h1>Account</h1>} />
+                  <Route
+                    path="/Dashboard"
+                    element={
+                      <Dashboard
+                        courses={coursesList}
+                        course={course}
+                        setCourse={setCourse}
+                        addNewCourse={addNewCourse}
+                        deleteCourse={deleteCourse}
+                        updateCourse={updateCourse}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/Courses/:courseId/*"
+                    element={<Courses courses={coursesList} />}
+                  />
+                  <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                  <Route path="/Inbox" element={<h1>Inbox</h1>} />
+                  <Route path="/History" element={<h1>History</h1>} />
+                  <Route path="/Studio" element={<h1>Studio</h1>} />
+                  <Route path="/Commons" element={<h1>Commons</h1>} />
+                  <Route path="/Help" element={<h1>Help</h1>} />
+                </Routes>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
   );
 }
 export default Kanbas;
